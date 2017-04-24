a7d31f5d483bcabe89e831d5874c1bc91b4091ca
Issifu Suhununu <issifu.suhununu@centre.edu>
2017-04-24 03:00:49 -0400

    landing page

---
+++ index.js
@@ -0,0 +1,114 @@
+( function()
+{
+    angular.module("assessment", ['angularBootstrapNavTree', 'ngAnimate', 'ngQuill']);
+    var genService = function(){
+        return ["pizza", "pasta", "potatoes", "carrots"]
+    };
+    var mainController = function($scope, genService, $sce){
+        var tree = $scope.my_tree = {};
+        $scope.tree_handler = function(branch){
+            console.log(branch.label)
+            $scope.selected = branch.label;
+        }
+        $scope.message = $sce.trustAsHtml($scope.message);
+        $scope.treedata_avm = [
+            {
+                label: 'Animal',
+                children: [
+                  {
+                    label: 'Dog',
+                    data: {
+                      description: "man's best friend"
+                    }
+                  }, {
+                    label: 'Cat',
+                    data: {
+                      description: "Felis catus"
+                    }
+                  }, {
+                    label: 'Hippopotamus',
+                    data: {
+                      description: "hungry, hungry"
+                    }
+                  }, {
+                    label: 'Chicken',
+                    children: ['White Leghorn', 'Rhode Island Red', 'Jersey Giant']
+                  }
+                ]
+            }, 
+            {
+                label: 'Vegetable',
+                data: {
+                  definition: "A plant or part of a plant used as food, typically as accompaniment to meat or fish, such as a cabbage, potato, carrot, or bean.",
+                  data_can_contain_anything: true
+                },
+                onSelect: function(branch) {
+                  return $scope.output = "Vegetable: " + branch.data.definition;
+                },
+                children: [
+                  {
+                    label: 'Oranges'
+                  }, {
+                    label: 'Apples',
+                    children: [
+                      {
+                        label: 'Granny Smith',
+                        onSelect: $scope.tree_handler
+                      }, {
+                        label: 'Red Delicous',
+                        onSelect: $scope.tree_handler
+                      }, {
+                        label: 'Fuji',
+                        onSelect: $scope.tree_handler
+                      }
+                    ]
+                  }
+                ]
+          }, 
+          {
+                label: 'Mineral',
+                children: [
+                  {
+                    label: 'Rock',
+                    children: ['Igneous', 'Sedimentary', 'Metamorphic']
+                  }, {
+                    label: 'Metal',
+                    children: ['Aluminum', 'Steel', 'Copper']
+                  }, {
+                    label: 'Plastic',
+                    children: [
+                      {
+                        label: 'Thermoplastic',
+                        children: ['polyethylene', 'polypropylene', 'polystyrene', ' polyvinyl chloride']
+                      }, {
+                        label: 'Thermosetting Polymer',
+                        children: ['polyester', 'polyurethane', 'vulcanized rubber', 'bakelite', 'urea-formaldehyde']
+                      }
+                    ]
+                  }
+                ]
+         },
+         {
+          label: 'Languages',
+          children: ['Jade','Less','Coffeescript']
+         }
+        ];
+        $scope.add_branch = function() {
+          var b;
+          b = tree.get_selected_branch();
+          return tree.add_branch(b, {
+            label: 'New Branch',
+            data: {
+              something: 42,
+              "else": 43
+            }
+          });
+        };
+    }
+    angular.module("assessment")
+        .controller("mainController", mainController)
+        .service("genService", genService)
+        .config(['ngQuillConfigProvider', function(ngQuillConfigProvider){
+            ngQuillConfigProvider.set(null, null, 'custom placeholder')
+        }])
+})();

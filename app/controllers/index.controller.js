3a0b21b698481242000102185907349774c307d4
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-01 16:44:16 -0400

    Update index controller to show stuff if you are logged in or not

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,46 +1,13 @@
 var Assessment = require('mongoose').model('Assessment');
 
-exports.render = function (req, res) {
-	var departments = [
-		'Anthropology/Sociology',
-		'Art History',
-		'Behavioral Neuroscience',
-		'Biochemistry & Molecular Biology',
-		'Biology',
-		'Chemical Physics',
-		'Chemistry',
-		'Classical Studies',
-		'Computer Science',
-		'Dramatic Arts',
-		'Economics and Finance',
-		'English',
-		'Environmental Studies',
-		'French',
-		'German Studies',
-		'History',
-		'International Studies',
-		'Mathematics',
-		'Music',
-		'Philosophy',
-		'Physics',
-		'Politics',
-		'Psychology',
-		'Religion',
-		'Spanish',
-		'Studio Art'
-	];
-
-	for (var i1 = 0; i1 < departments.length; i1++) {
-		var department = departments[i1];
-		var assessment = new Assessment({
-			department: department
-		});
-		assessment.save(function (err) {
-			if (err) {
-				console.log(err);
-			} else {
-				res.json(assessment);
-			}
-		})
+exports.render = function (req, res, next) {
+	var pageData = {title: 'Home'};
+	if (req.user) {
+		pageData.loggedIn = true;
+		pageData.department = req.user.department;
+	} else {
+		pageData.loggedIn = false;
 	}
+
+	res.render('index.ejs', pageData);
 };

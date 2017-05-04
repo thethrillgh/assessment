71a6f3357f652b95c31885033233e009f5acb562
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-04 09:58:34 -0400

    Create endpoint for getting all information for an evaluation

--- app/controllers/assessment.controller.js
+++ app/controllers/assessment.controller.js
@@ -38,14 +38,33 @@ exports.readAssessments = function (req, res, next) {
 	});
 };
 
-exports.read = function (req, res, next) {
-	var department = req.params.department;
-	var year = req.params.year;
-	Assessment.find({department: department}, function(err, assessments) {
-		if (err) {
-			next(err);
-		} else {
+/**
+ * Gets all the information for the evaluation for a specific year
+ */
+exports.readAssessment = function (req, res, next) {
+	var year = '2015-2017'; //TODO Get this as request parameter
 
-		}
-	});
+	if (req.user) {
+		var department = req.user.department;
+		Assessment.findOne({
+			department: department
+		}, 'evaluations', function (err, assessment) {
+			if (err) {
+				next(err);
+			} else {
+				var evaluations = assessment.evaluations;
+				var eval = evaluations.find(function (elem) {
+					return elem.year === year;
+				});
+				if (eval) {
+					res.json(eval);
+				} else {
+					res.json(error('No evaluation found for the given year'));
+				}
+			}
+		});
+	} else {
+		res.json(error(strings.notLoggedIn));
+	}
 };
+

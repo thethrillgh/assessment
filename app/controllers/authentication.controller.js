19fb26fa1a5b9ac2e191d84757699d9e29efcfef
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-04 10:00:35 -0400

    Sort departments in alphabetical order

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -1,7 +1,7 @@
 var Assessment = require('mongoose').model('Assessment');
 
 exports.renderSignin = function (req, res, next) {
-	Assessment.find({}, 'department', function (err, departments) {
+	Assessment.find({}, 'department', {sort: {department: 1}}, function (err, departments) {
 		if (!req.user) { //Not signed in
 			res.render('signin.ejs', {
 				messages: req.flash('error'),

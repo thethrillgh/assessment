7eaa732baf97f6ff447fe8a1ed605e0c3e2c143d
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-04 10:00:54 -0400

    Assessment routes working

--- app/routes/assessment.routes.js
+++ app/routes/assessment.routes.js
@@ -1,6 +1,10 @@
 var assessment = require('../controllers/assessment.controller');
 
 module.exports = function (app) {
+	app.get('/assessments', assessment.readAssessments);
+	app.get('/assessment', assessment.readAssessment);
+	app.post('/newAssessment', assessment.create);
+
 	//app.route('/assessments').get(assessment.readAssessments);
 	
 	//app.route('/assessments')

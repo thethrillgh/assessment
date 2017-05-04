e019ddbc1bb84924521219015112af057b60f641
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-04 09:58:09 -0400

    Add string for no user logged in

--- config/strings.js
+++ config/strings.js
@@ -1,4 +1,5 @@
 module.exports = {
 	noUserFound: 'There was no user found for that department.',
-	incorrectPassword: 'Your password was incorrect.'
+	incorrectPassword: 'Your password was incorrect.',
+	notLoggedIn: 'No user is logged in'
 }

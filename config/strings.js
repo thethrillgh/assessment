8f2f98b401af4b0b37341ae95ebf796ffd3f6059
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-06 14:05:20 -0400

    Add more strings to strings file

--- config/strings.js
+++ config/strings.js
@@ -1,5 +1,7 @@
 module.exports = {
 	noUserFound: 'There was no user found for that department.',
 	incorrectPassword: 'Your password was incorrect.',
-	notLoggedIn: 'No user is logged in'
+	notLoggedIn: 'No user is logged in',
+	noEvaluationFound: 'No evaluation was found for that year.',
+	noGoalFound: 'No goal with that id was found.'
 }

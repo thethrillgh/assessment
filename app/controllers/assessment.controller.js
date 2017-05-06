830101ab233c03f0528ba936d2e6d787198cb7dc
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-06 13:48:18 -0400

    Creating a new goal now working

--- app/controllers/assessment.controller.js
+++ app/controllers/assessment.controller.js
@@ -33,6 +33,59 @@ exports.create = function(req, res, next) {
 };
 
 /**
+ * Creates a new empty goal with an id for a given evaluation year
+ */
+exports.createGoal = function(req, res, next) {
+	var year = '2016-2017'; //TODO get this as a parameter
+
+	//Go ahead and generate id for the goal
+	var id = mongoose.Types.ObjectId();
+
+	//Init goal object with an empty label
+	var goalObj = {
+		_id: id,
+		label: '',
+		data: {
+			info: ''
+		}
+	};
+
+	if (req.user) {
+		Assessment.findOne({
+			department: 'Anthropology/Sociology' //req.user.department
+		}, 'evaluations', function (err, department) {
+			if (err) {
+				next(err);
+			} else {
+				var evaluations = department.evaluations;
+
+				//Find the evaluation for the given year
+				var evaluationForYear = evaluations.find(function (elem) {
+					return elem.year === year;
+				});
+
+				//Make sure the evaluation for given year exists
+				if (evaluationForYear) {
+					var evaluationObj = evaluationForYear.evaluation;
+					var goals = evaluationObj.children;
+
+					var goalsLen = goals.push(goalObj);
+					department.save(function (err) {
+						if (err) {
+							next(err);
+						} else {
+							res.json(success({goalId: id}));
+						}
+					});
+				} else {
+					res.json(error(strings.noEvaluationFound));
+				}
+			}
+		});
+	} else {
+		res.json(error(strings.notLoggedIn));
+	}
+};
  * Gets the list of years for the logged in user's department
  */
 exports.readAssessments = function (req, res, next) {

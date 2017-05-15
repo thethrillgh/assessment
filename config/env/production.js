7d7641d444d3df256eb7e9cc1060ba7b26a6a9ea
Jordan Turley <ztizzlegaming@gmail.com>
2017-05-15 15:19:45 -0400

    Create config file for production

---
+++ config/env/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+	db: 'mongodb://production:12345@ds123371.mlab.com:23371/assessment-production',
+	sessionSecret: 'productionSessionSecret' //This would probably be changed later I think
+};
